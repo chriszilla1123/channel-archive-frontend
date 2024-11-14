a72d84471457fbf7842e8446b060f75ce42d2b9c
chriszilla1123 <chriszilla1123@gmail.com>
2024-11-14 10:56:35 -0500

    Implement notifications using primeng toast

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,15 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { AuthInterceptor } from './interceptor/auth.interceptor';
+import {MessageService} from "primeng/api";
+import {provideAnimations} from "@angular/platform-browser/animations";
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes), 
-    provideHttpClient(withInterceptors([AuthInterceptor]))
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([AuthInterceptor])),
+    provideAnimations(),
+    MessageService
   ]
 };
---
+++ src/app/enum/NotificationLevel.enum.ts
@@ -0,0 +1,6 @@
+export enum NotificationLevel {
+  INFO = "info",
+  WARN = "warn",
+  SUCCESS = "success",
+  ERROR = "error",
+}
